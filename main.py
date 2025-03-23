b3c4a3fd2771da25ff940054cb07df44b64c738a
asadtalib <muhammadasad02012007@gmail.com>
2025-03-22 22:52:22 -0700

    changes

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-full_name = "Asad Talib"
+# question - 01
 
-print(f"Hello {full_name}")
+
+favourit_movies= ["KGF1","KGF2","Harry","Potter","Fast$Furious"]
+for movie in favourit_movies:
+    print(movie.title())
+    print(favourit_movies.index(movie), movie)
+
+
+    # question -02
+
+    full_name = "Muhammad asad"
+    vowel="aeiouAEIOU"
+    vowel_count =0;
+    for char in full_name:
+        print(char)
+        if char in vowel:
+         vowel_count +=1
+        print("char:",vowel_count)
+   
+
+
+
